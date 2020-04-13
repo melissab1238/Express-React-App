1f1f18cc943d151ba36329d7b159c4f98676c513
Melissa Bernstein <43127281+melissab1238@users.noreply.github.com>
2020-04-12 21:47:09 -0700

    trying to debug nodeenv

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { config } from "./Constants";
+import { config, nodeenv } from "./Constants";
 
 class App extends Component {
     constructor(props) {
@@ -11,6 +11,10 @@ class App extends Component {
 
     callAPI() {
         let api_url = config.url.API_URL
+        console.log(api_url)
+        console.log(process.env.NODE_ENV)
+        console.log(nodeenv)
+        
         fetch(api_url)
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
@@ -29,6 +33,7 @@ class App extends Component {
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">{this.state.apiResponse}</p>
+                <p className="App-intro">{process.env.
             </div>
         );
     }
